fc98865821b50a3084cc72d2635b253e0a181993
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-06-12 11:37:58 +0600

    6 feature food item fetching

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const app =express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const port =process.env.PORT ||5000
+const port = process.env.PORT || 5000;
 
-require('dotenv').config()
+require("dotenv").config();
 
-
-// middleware 
+// middleware
 app.use(cors());
-app.use(express.json())
-
-
-
- 
-
-
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shazid.sdvbyar.mongodb.net/?retryWrites=true&w=majority&appName=Shazid`;
 
@@ -27,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,26 +27,38 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const foodCollection = client.db("ShareBite").collection("foods");
 
+    // api
+    app.post("/foods", async (req, res) => {
+      const food = req.body;
+      const result = await foodCollection.insertOne(food);
+      res.send(result);
+    });
+    // 6 food item for feature 
 
-    const foodCollection=client.db("ShareBite").collection("foods")
+app.get("/featured-foods", async (req, res) => {
+  try {
+    const featuredFoods = await foodCollection
+      .find()
+      .sort({ quantity: -1 }) // descending order
+      .limit(6)
+      .toArray();
 
-    // api 
-    app.post('/foods',async (req,res)=>{
-      const food=req.body
-      const result=await foodCollection.insertOne(food)
-      res.send(result) 
-
-
-
-    })
+    res.send(featuredFoods);
+  } catch (error) {
+    res.status(500).send({ error: "Failed to fetch featured foods." });
+  }
+});
 
 
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -62,34 +66,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/' ,(req,res)=>{
-
-
-
+app.get("/", (req, res) => {
   res.send("Carrer code cooking");
+});
 
-
-})
-
-app.listen(port,()=>{
-
-
+app.listen(port, () => {
   console.log("shazid");
-  
-})
+});
